92b463cf36209e43fb1cc7ceb06b2ce54da1eadd
ebu <karae31@farmingdale.edu>
2024-09-04 19:17:15 -0400

    s

---
+++ src/main/java/com/mycompany/studentlst/Student.java
@@ -0,0 +1,28 @@
+package com.mycompany.studentlst;
+
+/**
+ *
+ * @author MoaathAlrajab
+ */
+public class Student {
+    
+    private String name;
+
+    // Constructor to initialize 'name'
+    public Student(String name) {
+        this.name = name;
+    }
+
+    // Getter for 'name'
+    public String getName() {
+        return name;
+    }
+
+    // Setter for 'name'
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+}
---
+++ src/main/java/com/mycompany/studentlst/StudentLst.java
@@ -0,0 +1,39 @@
+package com.mycompany.studentlst;
+
+import java.util.Scanner;
+
+/**
+ * @author MoaathAlrajab
+ */
+public class StudentLst {
+
+    public static void main(String[] args) {
+        Student[] students= new Student[2];
+        Scanner inScanner = new Scanner(System.in);
+        
+        for(int i=0; i< students.length;i++){
+            System.out.print("Enter student's name: ");
+            String name = inScanner.nextLine();
+            Student student = new Student(name);
+            
+            students[i]= student;
+            
+        }
+        System.out.println(printOutput(students));
+    }
+    
+    public static String printOutput(Student[] students){
+        String str = ""; // Initialize empty string
+
+        // array of students
+        for (Student student : students) {
+            if (student != null) { // NullPointerException check
+                str += student.getName() + "\n";
+            }
+        }
+
+        return str;
+    }
+    
+    
+}
